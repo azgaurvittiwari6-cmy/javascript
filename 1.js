8a76157341a06302bf298d58f10d00b8a8100fe9
azgaurvittiwari6-cmy <azgaurvittiwari6@gmail.com>
2025-12-05 19:43:49 +0530

    first commit

---
+++ 1.js
@@ -0,0 +1,26 @@
+//if
+const isUserLoggedIn = true;
+if (2 === "2") {
+    console.log("exacuted");
+
+
+
+
+}
+
+if (2 <= 2) {
+    console.log("exacuted");
+} else {
+    console.log("not exacuted");
+}
+
+
+if (2 == "2") {
+    console.log("exacuted");
+} else {
+    console.log("not exacuted");
+}
+
+
+
+

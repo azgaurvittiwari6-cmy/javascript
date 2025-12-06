289d0d7617d8a778a611314dba475e28f236a2c3
azgaurvittiwari6-cmy <azgaurvittiwari6@gmail.com>
2025-12-06 15:41:13 +0530

    TodayI I learn anout function

---
+++ 2.js
@@ -0,0 +1,12 @@
+function name() {
+    console.log("H");
+    console.log("E");
+    console.log("L");
+    console.log("L");
+    console.log("O");
+}
+function addTwoNumbers(number1, number2) {
+    console.log(number1 + number2);
+
+}
+addTwoNumbers(12, 7);    

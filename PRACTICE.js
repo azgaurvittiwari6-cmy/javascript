c202e7da1c75e886d16b7a296e206df21b1c8c67
azgaurvittiwari6-cmy <azgaurvittiwari6@gmail.com>
2026-02-17 16:12:13 +0530

    Practice of Local Storage

---
+++ PRACTICE.js
@@ -0,0 +1,64 @@
+//Var,let and const
+let a= 23;
+let b = 24
+let c = a+b;
+console.log(c);
+//for loop
+
+for(i = 0; i<20,i++;){
+    console.log("Hello");
+}
+
+//if else
+
+    if(23<40){
+        console.log("🔥🔥");
+        
+    }else{
+        console.log("Work hard");
+        
+    }
+
+    //&& 
+    if(23>40 && 23>50 ){
+        console.log("False");
+        
+    }else{
+        console.log("True");
+        
+    }
+
+    //function
+//  A simple function of  adding 2 numbers
+
+function el  () {
+    const num1 = 1000
+    const num2 = 100000
+     const num3 = num1+num2
+     console.log(num3);
+}
+el()
+
+//arrow function and callback function
+
+const ele  =  ()=>{
+    console.log("Konnichiva");
+    
+}
+ele()
+
+//documentation
+
+const btn = document.getElementById()
+const button = document.getElementById()
+
+//eventLister
+
+const btn1 = document.getElementById("btn")
+
+btn.addEventListener(){
+    alert("heyy")
+
+}
+
+
